c85cb0bfe0b8f0db94a18d968aeb63a6cbdfe8fa
Su Zhang <0x82cf@gmail.com>
2011-09-27 01:28:21 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Sudoku
+{
+    partial class Sudoku
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab
+            // 
+            this.tab.Controls.Add(this.tabPage1);
+            this.tab.Controls.Add(this.tabPage2);
+            this.tab.Location = new System.Drawing.Point(12, 12);
+            this.tab.Name = "tab";
+            this.tab.SelectedIndex = 0;
+            this.tab.Size = new System.Drawing.Size(342, 405);
+            this.tab.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(334, 379);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Puzzle";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(249, 345);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 345);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Solution";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(334, 379);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Solver";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            //this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(207, 345);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(45, 345);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Solve";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(126, 345);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // boxes
+            this.digitBoxes = new System.Windows.Forms.MaskedTextBox[9][];
+            this.digitBoxes2 = new System.Windows.Forms.MaskedTextBox[9][];
+            for (byte i = 0; i < 9; ++i)
+            {
+                this.digitBoxes[i] = new System.Windows.Forms.MaskedTextBox[9];
+                this.digitBoxes2[i] = new System.Windows.Forms.MaskedTextBox[9];
+                for (byte j = 0; j < 9; ++j)
+                {
+                    byte index = (byte)(i * 9 + j);
+                    this.digitBoxes[i][j] = new System.Windows.Forms.MaskedTextBox();
+                    this.digitBoxes[i][j].Font = new System.Drawing.Font("Courier New", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    this.digitBoxes[i][j].Location = new System.Drawing.Point(5 + j * 35 + j / 3 * 3, 12 + i * 35 + i / 3 * 3);
+                    this.digitBoxes[i][j].Mask = "9";
+                    this.digitBoxes[i][j].Name = "maskedTextBox" + index;
+                    this.digitBoxes[i][j].Size = new System.Drawing.Size(35, 35);
+                    this.digitBoxes[i][j].HidePromptOnLeave = true;
+                    this.digitBoxes[i][j].TabIndex = 5 + index;
+                    this.digitBoxes[i][j].Enter += new System.EventHandler(digitBoxes_Enter);
+                    this.digitBoxes[i][j].Click += new System.EventHandler(digitBoxes_Enter);
+                    this.tabPage1.Controls.Add(this.digitBoxes[i][j]);
+                    this.digitBoxes2[i][j] = new System.Windows.Forms.MaskedTextBox();
+                    this.digitBoxes2[i][j].Font = new System.Drawing.Font("Courier New", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    this.digitBoxes2[i][j].Location = new System.Drawing.Point(5 + j * 35 + j / 3 * 3, 12 + i * 35 + i / 3 * 3);
+                    this.digitBoxes2[i][j].Mask = "9";
+                    this.digitBoxes2[i][j].Name = "maskedTextBox" + index;
+                    this.digitBoxes2[i][j].Size = new System.Drawing.Size(35, 35);
+                    this.digitBoxes2[i][j].HidePromptOnLeave = true;
+                    this.digitBoxes2[i][j].TabIndex = 5 + index;
+                    this.digitBoxes2[i][j].Click += new System.EventHandler(digitBoxes_Enter);
+                    this.tabPage1.Controls.Add(this.digitBoxes[i][j]);
+                    this.tabPage2.Controls.Add(this.digitBoxes2[i][j]);
+                }
+            }
+            // 
+            // Sudoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 429);
+            this.Controls.Add(this.tab);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;;
+            this.MaximizeBox = false;
+            this.Name = "Sudoku";
+            this.Text = "Sudoku";
+            this.tab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox[][] digitBoxes;
+        private System.Windows.Forms.MaskedTextBox[][] digitBoxes2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tab;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Sudoku
+{
+    public partial class Sudoku : Form
+    {
+        public Sudoku()
+        {
+            InitializeComponent();
+            newPuzzle();
+        }
+
+        private void digitBoxes_Enter(object sender, EventArgs e)
+        {
+            ((MaskedTextBox)sender).SelectionStart = 0;
+            ((MaskedTextBox)sender).SelectionLength = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newPuzzle();
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (byte i = 0; i < 9; ++i)
+            {
+                for (byte j = 0; j < 9; ++j)
+                {
+                    if (digitBoxes[i][j].Enabled && (digitBoxes[i][j].Text.Length < 1 || digitBoxes[i][j].Text[0] != board[i][j]))
+                    {
+                        MessageBox.Show(this, "Row " + (i + 1) + " Col " + (j + 1) + " is wrong!", "Sudoku");
+                        digitBoxes[i][j].Focus();
+                        return;
+                    }
+                }
+            }
+            MessageBox.Show("Congratulation, you win!", "Sudoku");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (byte i = 0; i < 9; ++i)
+            {
+                for (byte j = 0; j < 9; ++j)
+                {
+                    if (digitBoxes[i][j].Enabled)
+                    {
+                        digitBoxes[i][j].Text = "" + board[i][j];
+                        digitBoxes[i][j].Enabled = false;
+                    }
+                }
+            }
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            char[][] board = new char[9][];
+            for(byte i = 0; i < 9; ++i)
+            {
+                board[i] = new char[9];
+                for(byte j = 0; j < 9; ++j)
+                {
+                    if(digitBoxes2[i][j].Text.Length > 0)
+                    {
+                        board[i][j] = digitBoxes2[i][j].Text[0];
+                    }
+                    else
+                    {
+                        board[i][j] = '\0';
+                    }
+                }
+            }
+            if (!solve(board, 0, 0))
+            {
+                MessageBox.Show("No solution!");
+                return;
+            }
+            for (byte i = 0; i < 9; ++i)
+            {
+                for (byte j = 0; j < 9; ++j)
+                {
+                    digitBoxes2[i][j].Text = "" + board[i][j];
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            for (byte i = 0; i < 9; ++i)
+            {
+                for (byte j = 0; j < 9; ++j)
+                {
+                    digitBoxes2[i][j].Text = "";
+                }
+            }
+        }
+
+        private ArrayList getValidDigits(char[][] board, byte row, byte col)
+        {
+            ArrayList digits = new ArrayList();
+            for (char i = '1'; i <= '9'; ++i)
+            {
+                digits.Add(i);
+            }
+            // check rows and columns
+            for (byte i = 0; i < 9; ++i)
+            {
+                if (i != col && board[row][i] > 0)
+                    digits.Remove(board[row][i]);
+                if (i != row && board[i][col] > 0)
+                    digits.Remove(board[i][col]);
+            }
+            // check sub-box
+            byte r = (byte)(row / 3 * 3);
+            byte c = (byte)(col / 3 * 3);
+            for (byte i = r; i < r + 3; ++i)
+            {
+                for (byte j = c; j < c + 3; ++j)
+                {
+                    if (i == row || j == col) continue;
+                    if (board[i][j] > 0)
+                        digits.Remove(board[i][j]);
+                }
+            }
+            return digits;
+        }
+
+        private bool solve(char[][] board, byte row, byte col)
+        {
+            if (row == 9) return true;
+            ArrayList digits = getValidDigits(board, row, col);
+            if (board[row][col] > 0)
+            {
+                if(digits.Contains(board[row][col]))
+                    return col == 8 ? solve(board, (byte)(row + 1), 0) : solve(board, row, (byte)(col + 1));
+                else return false;
+            }
+            for(byte i = 0; i < digits.Count; ++i)
+            {
+                board[row][col] = (char)digits[i];
+                if (col == 8 ? solve(board, (byte)(row + 1), 0) : solve(board, row, (byte)(col + 1)))
+                {
+                    return true;
+                }
+            }
+            board[row][col] = '\0';
+            return false;
+        }
+
+        private bool generate()
+        {
+            Random r = new Random();
+            ArrayList digits = new ArrayList() { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            board = new char[9][];
+            for (byte i = 0; i < 9; ++i)
+            {
+                board[i] = new char[9];
+                for (byte j = 0; j < 9; ++j)
+                {
+                    board[i][j] = '\0';
+                }
+            }
+            // initialize a random row
+            for (byte i = 8; i > 1; --i)
+            {
+                byte p = (byte)r.Next(i);
+                object temp = digits[i];
+                digits[i] = digits[p];
+                digits[p] = temp;
+            }
+            for (byte i = 0; i < 9; ++i)
+            {
+                board[0][i] = (char)digits[i];
+            }
+            // also initialize a random sub-box
+            digits.Remove(board[0][0]);
+            digits.Remove(board[0][1]);
+            digits.Remove(board[0][2]);
+            for (byte i = 5; i > 1; --i)
+            {
+                byte p = (byte)r.Next(i);
+                object temp = digits[i];
+                digits[i] = digits[p];
+                digits[p] = temp;
+            }
+            for (byte i = 0; i < 6; ++i)
+            {
+                board[i / 3 + 1][i % 3] = (char)digits[i];
+            }
+            return solve(board, 0, 0);
+        }
+
+        private void newPuzzle()
+        {
+            Random r = new Random();
+            generate();
+            byte[] randoms = new byte[81];
+            for (byte i = 0; i < 81; ++i)
+            {
+                randoms[i] = i;
+            }
+            // generate random blank positions
+            for (byte i = 80; i > 1; --i)
+            {
+                byte p = (byte)r.Next(i);
+                randoms[i] ^= randoms[p];
+                randoms[p] ^= randoms[i];
+                randoms[i] ^= randoms[p];
+            }
+            // disable all boxes
+            for (byte i = 0; i < 9; ++i)
+            {
+                for (byte j = 0; j < 9; ++j)
+                {
+                    digitBoxes[i][j].Enabled = false;
+                    digitBoxes[i][j].Text = "";
+                }
+            }
+            // enable random boxes for player to fill in
+            for (byte i = 0; i < NUMBLANK; ++i)
+            {
+                digitBoxes[randoms[i] / 9][randoms[i] % 9].Enabled = true;
+            }
+            for (byte i = 0; i < 9; ++i)
+            {
+                for (byte j = 0; j < 9; ++j)
+                {
+                    if (!digitBoxes[i][j].Enabled)
+                    {
+                        digitBoxes[i][j].Text = "" + board[i][j];
+                    }
+                }
+            }
+        }
+
+        private const byte NUMBLANK = 47;
+        private char[][] board;
+    }
+}
